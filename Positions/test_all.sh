4bd201ff1b49a85b9a0190bc6d517654eb4f23aa
Niceboy5275 <vdesoutter@gmail.com>
2017-07-04 16:45:28 +0200

    Create test_all.sh

---
+++ Positions/test_all.sh
@@ -0,0 +1,18 @@
+for file in `ls -1 solution*.txt`
+do
+    python ../classes/chess_simple.py < $file > output_$file
+    nbLines=`cat output_$file | grep "Echec et mat !" | wc -l`
+    if [ $nbLines -ne 1 ]
+    then
+        echo 'Error with file '$file
+    fi
+    output_ref=`echo "output_$file" | sed 's/txt/ref/g'`
+    nbLines=`diff  output_$file $output_ref | wc -l`
+    if [ $nbLines -ne 0 ]
+    then
+        diff  output_$file $output_ref
+        echo 'Error with file '$file
+    else
+        rm output_$file
+    fi
+done
