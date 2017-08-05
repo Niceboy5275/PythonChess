06c16997c7c0c262f6cad6211c3823a54f792a8e
Vincent Desoutter <v.desoutter@gmail.com>
2017-08-05 16:06:04 +0200

    New test cases 163 - 166

--- Positions/test_all.sh
+++ Positions/test_all.sh
@@ -1,20 +1,82 @@
-for file in `ls -1 solution*.txt`
+export PATH=/c/Users/Vincent/AppData/Local/Programs/Python/Python36:$PATH
+for file in `ls -1 | egrep "^solution_[0-9]+.txt"`
 do
-    python ../classes/chess_simple.py < $file > output_$file
-    nbLines=`cat output_$file | grep "Echec et mat !" | wc -l`
+    echo -n '.'
+    python ../classes/chess_main.py < $file > output_simple_$file
+    nbLines=`cat output_simple_$file | grep "Echec et mat !" | wc -l`
     if [ $nbLines -ne 1 ]
     then
-        echo 'Error with file '$file
+        echo -n 'F1S'
     fi
-    output_ref=`echo "output_$file" | sed 's/txt/ref/g'`
-    dos2unix output_$file
-    dos2unix $output_ref
-    nbLines=`diff  output_$file $output_ref | wc -l`
+    output_ref=`echo "output_simple_$file" | sed 's/txt/ref/g'`
+    touch $output_ref
+    dos2unix $output_ref 2> /dev/null
+    dos2unix output_simple_$file 2> /dev/null
+    nbLines=`diff  output_simple_$file $output_ref | wc -l`
     if [ $nbLines -ne 0 ]
     then
-        diff  output_$file $output_ref
-        echo 'Error with file '$file
+        mv output_simple_$file $output_ref
+        echo -n 'F2S'
     else
-        rm output_$file
+        rm output_simple_$file
+    fi
+done
+echo ""
+for file in `ls -1 solution_*.txt | egrep -v "^solution_[0-9]+.txt"`
+do
+    echo -n '.'
+    python ../classes/chess_main.py < $file > output_simple_$file
+    output_ref=`echo "output_simple_$file" | sed 's/txt/ref/g'`
+    touch $output_ref
+    dos2unix $output_ref 2> /dev/null
+    dos2unix output_simple_$file 2> /dev/null
+    nbLines=`diff  output_simple_$file $output_ref | wc -l`
+    if [ $nbLines -ne 0 ]
+    then
+        mv output_simple_$file $output_ref
+        echo -n 'F2S'
+    else
+        rm output_simple_$file
+    fi
+done
+echo ""
+for file in `ls -1 | egrep "^solution_[0-9]+.txt"`
+do
+    echo -n '.'
+    python ../classes/chess_main.py linux < $file > output_linux_$file
+    nbLines=`cat output_linux_$file | grep "Echec et mat !" | wc -l`
+    if [ $nbLines -ne 1 ]
+    then
+        echo -n 'F1L'
+    fi
+    output_ref=`echo "output_linux_$file" | sed 's/txt/ref/g'`
+    touch $output_ref
+    dos2unix $output_ref 2> /dev/null
+    dos2unix output_linux_$file 2> /dev/null
+    nbLines=`diff  output_linux_$file $output_ref | wc -l`
+    if [ $nbLines -ne 0 ]
+    then
+        mv output_linux_$file $output_ref
+        echo -n 'F2L'
+    else
+        rm output_linux_$file
+    fi
+done
+echo ""
+for file in `ls -1 solution_*.txt | egrep -v "^solution_[0-9]+.txt"`
+do
+    echo -n '.'
+    python ../classes/chess_main.py < $file > output_linux_$file
+    output_ref=`echo "output_linux_$file" | sed 's/txt/ref/g'`
+    touch $output_ref
+    dos2unix $output_ref 2> /dev/null
+    dos2unix output_linux_$file 2> /dev/null
+    nbLines=`diff  output_linux_$file $output_ref | wc -l`
+    if [ $nbLines -ne 0 ]
+    then
+        mv output_linux_$file $output_ref
+        echo -n 'F2L'
+    else
+        rm output_linux_$file
     fi
 done
